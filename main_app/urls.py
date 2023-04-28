f830a26c6e42f1d22b8714d561850e379551929e
arsenhreskiv <harsen17mu@gmail.com>
2023-04-28 13:46:35 -0500

    Arsen Driving: added pet edit and delete, added vet functions and made wthem work

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,15 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('pets/', views.PetList.as_view(), name='pet_index'),
-    path('pets/<int:pk>', views.PetDetailView.as_view(), name='detail'),
-    path('pets/create', views.PetCreateView.as_view(), name='pet_create'),
+    path('pets/<int:pk>/', views.PetDetailView.as_view(), name='pet_detail'),
+    path('pets/create/', views.PetCreateView.as_view(), name='pet_create'),
+    path('pets/<int:pk>/update/', views.PetUpdateView.as_view(), name='pet_update'),
+    path('pets/<int:pk>/delete/', views.PetDeleteView.as_view(), name='pet_delete'),
+    path('pets/<int:pk>/addvaccination/', views.PetAddVaccinationView.as_view(), name='pet_addvaccination'),
+    path('vets/', views.VetList.as_view(), name='vet_index'),
+    path('vets/<int:pk>/', views.VetDetailView.as_view(), name='vet_detail'),
+    path('vets/create/', views.VetCreateView.as_view(), name='vet_create'),
+    path('vets/<int:pk>/update/', views.VetUpdateView.as_view(), name='vet_update'),
+    path('vets/<int:pk>/delete/', views.VetDeleteView.as_view(), name='vet_delete'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 # Create your views here.
@@ -40,3 +40,36 @@ class PetCreateView(CreateView):
   def form_valid(self, form):
    form.instance.user = self.request.user
    return super().form_valid(form)
+
+class PetUpdateView(UpdateView):
+  model = Pet
+  fields = ['name', 'species', 'breed', 'birth_date']
+
+class PetDeleteView(DeleteView):
+  model = Pet
+  success_url = '/pets'
+
+class PetAddVaccinationView(CreateView):
+  model = Vaccination
+  fields = '__all__'
+  success_url = '/pets'
+  # def form_valid(self, form):
+  #  form.instance.user = self.request.user
+  #  return super().form_valid(form)
+
+class VetList(ListView):
+  model = Vet
+
+class VetDetailView(DetailView):
+  model = Vet
+
+class VetCreateView(CreateView):
+  model = Vet
+  fields = '__all__'
+
+class VetUpdateView(UpdateView):
+  model = Vet
+  fields = '__all__'
+class VetDeleteView(DeleteView):
+  model = Vet
+  success_url = '/vets'
